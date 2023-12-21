c5fa84961edf4b9a5c39e541f6ba5c32e32084a4
VietTitans <mattnhudinh1@gmail.com>
2023-12-21 08:36:44 +0100

    Completed planning  and pseudo code

--- myscrips.js
+++ myscrips.js
@@ -5,12 +5,20 @@ Initialize
 function getComputerChoice()
     run random.generator(choice)
     set random.generator to computerChoice
+    
     return computerChoice
 
 function getUserInput()
     get userinput (give example on input field "Write either rock, paper, scissors")
     if userInput is not equal to either rock, paper or scissors then print "Invalid input. Please write either rock, paper, scissors"
+    ENDIF
     return userInput
 
 function playGame()
-    if rock 
+    if userInput is equal to rock AND computerChoice is equal to scissors then print "You win"
+        else if userInput is equal to paper AND computerChoice is equal to rock then print "You win"
+        else if userInput is equal to scissors AND computerChoice is equal to paper then print "You win"
+        else if userInput is equal to computerChoice print "It's a tie"
+        else print "You lose"
+    ENDIF
+    return score
